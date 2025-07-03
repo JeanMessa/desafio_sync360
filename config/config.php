14d966c3042a50f46efb6640db1a0d2e945d6a73
JeanMessa <jeancarlosmessa@gmail.com>
2025-07-03 15:17:05 -0300

    feat: add config.php

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+    global $config;
+
+    $config = array();
+    define("BASE_URL","http://localhost/api/");
+    $config['dbname'] = 'sync360';
+    $config['host']   = 'localhost';
+    $config['dbuser'] = 'root';
+    $config['dbpass'] = '';
+?>
