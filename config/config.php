f35f24245ad29580cd6fc77b1932e7d7b438b2fc
JeanMessa <jeancarlosmessa@gmail.com>
2025-07-03 15:28:30 -0300

    fix: fix base url

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
     global $config;
 
     $config = array();
-    define("BASE_URL","http://localhost/api/");
+    define("BASE_URL","http://localhost/desafio_sync360/");
     $config['dbname'] = 'sync360';
     $config['host']   = 'localhost';
     $config['dbuser'] = 'root';
