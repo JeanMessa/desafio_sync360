6797ca0e70a423b5c07cd02a35cb7ecb416bcb08
JeanMessa <jeancarlosmessa@gmail.com>
2025-07-03 15:38:02 -0300

    fix: fix router errors

--- core/Router.php
+++ core/Router.php
@@ -3,12 +3,12 @@
         private $routes = array();
 
         public function addRoute($route,$callback){
-            $this->$routes[$route] = $callback;
+            $this->routes[$route] = $callback;
         }
 
-        public function handleRequest(){
-            if(array_key_exists($route, $this->$routes)){
-                $callback = $this->$routes[$route];
+        public function handleRequest($route){
+            if(array_key_exists($route, $this->routes)){
+                $callback = $this->routes[$route];
                 if(is_callable($callback)){
                     call_user_func($callback);
                 }else{
