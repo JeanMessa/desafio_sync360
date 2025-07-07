7a2b6a138809a002d7289eb77fb7fa2359310281
JeanMessa <jeancarlosmessa@gmail.com>
2025-07-07 14:53:02 -0300

    feat: allow access to frontend

--- api/core/Core.php
+++ api/core/Core.php
@@ -4,6 +4,11 @@ class Core{
 
     public function exec(){
 
+        header("Access-Control-Allow-Origin: http://localhost:4200");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
+        header("Access-Control-Max-Age: 3600");
+
         $router = new Router();
 
         $router->addRoute('/usuario',array(new UserController, 'main'));
